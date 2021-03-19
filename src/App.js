872dbd587252b3486924b41bc10f88ea94622a55
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-03-20 03:31:40 +0530

    feat: added nav and placeholder dash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import Dashboard from './pages/Dashboard'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#15131e",
+      light: '#1d1d27', 
+    },
+    background: {
+      default: "#15131e",
+    },
+    text: {
+      primary: "#f4f4f6",
+    },
+  }
+});
+
 function App() {
   return (
     <>
-      Gandalf
+      <Dashboard theme={theme}/>
     </>
   );
 }
