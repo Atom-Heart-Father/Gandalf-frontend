7e62b8f6a5a730ea94208b96c0234cf74af052b6
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-03-20 10:30:52 +0530

    fix: removed material and start from scratch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import Dashboard from './pages/Dashboard'
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#15131e",
-      light: '#1d1d27', 
-    },
-    background: {
-      default: "#15131e",
-    },
-    text: {
-      primary: "#f4f4f6",
-    },
-  }
-});
-
-function App() {
+const App = () => {
   return (
     <>
-      <Dashboard theme={theme}/>
+      Gandalf
     </>
   );
 }
